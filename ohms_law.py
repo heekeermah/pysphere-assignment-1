d9c143f69114a16d065061988873d8fe3eef4fb2
heekeermah <hikmayahya48@gmail.com>
2025-08-11 11:31:48 +0100

    Add files via upload

---
+++ ohms_law.py
@@ -0,0 +1,5 @@
+current = float(input('Enter the current (in A): '))
+resistance = float(input('Enter the resistance (in ohms): '))
+voltage = current*resistance
+print(f'The Voltage is: {voltage} volts.')
+print ('Voltage calculator by heekermah')
